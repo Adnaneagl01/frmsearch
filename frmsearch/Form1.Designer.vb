d2e18ad2e666cbc9877b40ff2eb7d7df77ad749e
adnan <adnan@LAPTOP-2CRK3QTD>
2022-01-01 12:45:14 +0100

    Add project files.

---
+++ frmsearch/Form1.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ville = New System.Windows.Forms.TextBox()
+        Me.prenom = New System.Windows.Forms.TextBox()
+        Me.nom = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.viller = New System.Windows.Forms.RadioButton()
+        Me.prenomr = New System.Windows.Forms.RadioButton()
+        Me.nomr = New System.Windows.Forms.RadioButton()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.ville)
+        Me.GroupBox1.Controls.Add(Me.prenom)
+        Me.GroupBox1.Controls.Add(Me.nom)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 28)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(770, 86)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "creation de recherche"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(500, 38)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(26, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Ville"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(265, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Prenom"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Nom"
+        '
+        'ville
+        '
+        Me.ville.Location = New System.Drawing.Point(559, 32)
+        Me.ville.Name = "ville"
+        Me.ville.Size = New System.Drawing.Size(149, 20)
+        Me.ville.TabIndex = 2
+        '
+        'prenom
+        '
+        Me.prenom.Location = New System.Drawing.Point(329, 32)
+        Me.prenom.Name = "prenom"
+        Me.prenom.Size = New System.Drawing.Size(149, 20)
+        Me.prenom.TabIndex = 3
+        '
+        'nom
+        '
+        Me.nom.Location = New System.Drawing.Point(90, 32)
+        Me.nom.Name = "nom"
+        Me.nom.Size = New System.Drawing.Size(149, 20)
+        Me.nom.TabIndex = 4
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.viller)
+        Me.GroupBox2.Controls.Add(Me.prenomr)
+        Me.GroupBox2.Controls.Add(Me.nomr)
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 130)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(770, 86)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Trier par"
+        '
+        'viller
+        '
+        Me.viller.AutoSize = True
+        Me.viller.Location = New System.Drawing.Point(549, 38)
+        Me.viller.Name = "viller"
+        Me.viller.Size = New System.Drawing.Size(44, 17)
+        Me.viller.TabIndex = 11
+        Me.viller.TabStop = True
+        Me.viller.Text = "Ville"
+        Me.viller.UseVisualStyleBackColor = True
+        '
+        'prenomr
+        '
+        Me.prenomr.AutoSize = True
+        Me.prenomr.Location = New System.Drawing.Point(316, 38)
+        Me.prenomr.Name = "prenomr"
+        Me.prenomr.Size = New System.Drawing.Size(61, 17)
+        Me.prenomr.TabIndex = 10
+        Me.prenomr.TabStop = True
+        Me.prenomr.Text = "Prenom"
+        Me.prenomr.UseVisualStyleBackColor = True
+        '
+        'nomr
+        '
+        Me.nomr.AutoSize = True
+        Me.nomr.Location = New System.Drawing.Point(122, 38)
+        Me.nomr.Name = "nomr"
+        Me.nomr.Size = New System.Drawing.Size(47, 17)
+        Me.nomr.TabIndex = 9
+        Me.nomr.TabStop = True
+        Me.nomr.Text = "Nom"
+        Me.nomr.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(11, 222)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(777, 209)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 526)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ville As TextBox
+    Friend WithEvents prenom As TextBox
+    Friend WithEvents nom As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents viller As RadioButton
+    Friend WithEvents prenomr As RadioButton
+    Friend WithEvents nomr As RadioButton
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ frmsearch/Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Dim cnx As New SqlConnection("database=GestCom2021; server=LAPTOP-2CRK3QTD;integrated security = true")
+    Dim cmd As New SqlCommand
+    Dim dr As SqlDataReader
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DataGridView1.Columns.Add("Idclient", "ID CLIENT")
+        DataGridView1.Columns.Add("nom", "Nom")
+        DataGridView1.Columns.Add("prenom", "Prenom")
+        DataGridView1.Columns.Add("tel", "telephone")
+        DataGridView1.Columns.Add("email", "E-mail")
+        DataGridView1.Columns.Add("pays", "Pays")
+        DataGridView1.Columns.Add("ville", "Ville")
+        DataGridView1.Columns.Add("codep", "Code Postal")
+        load_client()
+
+
+    End Sub
+    Private Sub load_client()
+        Try
+
+            cnx.Open()
+            cmd.Connection = cnx
+
+            charger_DataGridView1()
+
+        Catch e As Exception
+            MessageBox.Show(e.Message)
+
+        End Try
+    End Sub
+
+    Private Sub charger_DataGridView1()
+        Try
+            DataGridView1.Rows.Clear()
+            cmd.CommandText = "SELECT * FROM client"
+            dr = cmd.ExecuteReader
+            While dr.Read
+                DataGridView1.Rows.Add(dr(0), dr(1), dr(2), dr(3), dr(4), dr(5), dr(6), dr(7))
+            End While
+            dr.Close()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+
+    End Sub
+    Private Sub search()
+        cmd.CommandText = "select * from client where nom like '" & nom.Text & "%' and
+        prenom like '" & prenom.Text & "%' and
+        ville like '" & ville.Text & "%'"
+        dr = cmd.ExecuteReader
+        DataGridView1.Rows.Clear()
+        While dr.Read
+            DataGridView1.Rows.Add(dr(0), dr(1), dr(2), dr(3), dr(4), dr(5), dr(6), dr(7))
+
+        End While
+        dr.Close()
+    End Sub
+
+
+
+    Private Sub nom_TextChanged(sender As Object, e As EventArgs) Handles nom.TextChanged
+        search()
+    End Sub
+
+    Private Sub prenomr_CheckedChanged(sender As Object, e As EventArgs) Handles prenomr.CheckedChanged
+        trier("prenom", "asc")
+    End Sub
+
+    Private Sub prenom_TextChanged(sender As Object, e As EventArgs) Handles prenom.TextChanged
+        search()
+    End Sub
+
+    Private Sub ville_TextChanged(sender As Object, e As EventArgs) Handles ville.TextChanged
+        search()
+    End Sub
+
+    Private Sub nomr_CheckedChanged(sender As Object, e As EventArgs) Handles nomr.CheckedChanged
+        trier("nom", "asc")
+    End Sub
+
+    Private Sub viller_CheckedChanged(sender As Object, e As EventArgs) Handles viller.CheckedChanged
+        trier("ville", "asc")
+
+    End Sub
+    Private Sub trier(c As String, g As String)
+        cmd.CommandText = "select * from client where nom like '" & nom.Text & "%' and
+        prenom like '" & prenom.Text & "%' and
+        ville like '" & ville.Text & "%' Order by " & c & " " & g & ""
+        dr = cmd.ExecuteReader
+        DataGridView1.Rows.Clear()
+        While dr.Read
+            DataGridView1.Rows.Add(dr(0), dr(1), dr(2), dr(3), dr(4), dr(5), dr(6), dr(7))
+
+        End While
+        dr.Close()
+    End Sub
+End Class
